c69215c2cf8e4251ac321fa3f1df6683545877a7
AaronFacoline <apfacoline@tutanota.com>
2025-10-17 19:17:22 +0200

    Initial Commit

---
+++ src/main/java/za/co/connected/CashConnectAssignment/CashConnectAssignmentApplication.java
@@ -0,0 +1,13 @@
+package za.co.connected.CashConnectAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashConnectAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashConnectAssignmentApplication.class, args);
+	}
+
+}
